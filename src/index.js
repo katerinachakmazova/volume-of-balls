84a8dabb9a6fea2ef100defa108b972b673591f0
Катерина Чакмазова <chakmazova06@gmail.com>
2025-01-04 19:48:08 +0200

    Create an algorythm to count volume

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let diameter = 0.5;
+let thicknessOfWalls = 0.01;
+let volume = 0;
+let numberOfBalls = 12;
+
+let finalDiameter = diameter + 2 * thicknessOfWalls * numberOfBalls;
+
+for (diameter; diameter <= finalDiameter; diameter+=thicknessOfWalls*2) {
+    volume+=4/3*Math.PI*((diameter/2)**3);
+}
+
+console.log(`Total volume of ${numberOfBalls} balls is ${volume} metres`);
+
