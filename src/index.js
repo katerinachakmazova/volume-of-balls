7ac87540599e9775db5b5e6182f295252790a466
Катерина Чакмазова <chakmazova06@gmail.com>
2025-01-08 18:18:29 +0200

    Alter calculations and loop according to the assignment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 'use strict';
 
-let diameter = 0.5;
 let thicknessOfWalls = 0.01;
+let diameter = 0.5-2*thicknessOfWalls;
 let volume = 0;
 let numberOfBalls = 12;
 
-let finalDiameter = diameter + 2 * thicknessOfWalls * numberOfBalls;
-
-for (diameter; diameter <= finalDiameter; diameter+=thicknessOfWalls*2) {
+for (let i=0; i<12; i++) {
     volume+=4/3*Math.PI*((diameter/2)**3);
+    diameter+=thicknessOfWalls*2;  
 }
 
 console.log(`Total volume of ${numberOfBalls} balls is ${volume} metres`);
